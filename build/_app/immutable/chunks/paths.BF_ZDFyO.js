27c8c77836f1985f79b9ebc5fd8c759c456b63f7
jakedel <42163241+jakedel@users.noreply.github.com>
2025-01-20 00:51:04 -0800

    add 404 fallback

--- build/_app/immutable/chunks/paths.BF_ZDFyO.js
+++
@@ -1,1 +0,0 @@
-var s,e;const o=(e=(s=globalThis.__sveltekit_1hoehf7)==null?void 0:s.base)!=null?e:"";var a,t;const h=(t=(a=globalThis.__sveltekit_1hoehf7)==null?void 0:a.assets)!=null?t:o;export{h as a,o as b};
--- build/_app/immutable/entry/start.BHtcenqu.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CNsJEeww.js";export{t as start};
