a337d96a5049a3e7afe986c14a28057ed863e922
jakedel <42163241+jakedel@users.noreply.github.com>
2025-01-20 03:54:50 -0800

    fix konpu hero

---
+++ _app/immutable/chunks/paths.Dv0PMy-t.js
@@ -0,0 +1,1 @@
+var s,a;const l=(a=(s=globalThis.__sveltekit_1wrakty)==null?void 0:s.base)!=null?a:"";var t,e;const _=(e=(t=globalThis.__sveltekit_1wrakty)==null?void 0:t.assets)!=null?e:l;export{_ as a,l as b};
