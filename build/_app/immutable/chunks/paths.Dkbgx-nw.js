c43bd469db2b30daf82ac8ed7fc44fa189f86d2c
jakedel <42163241+jakedel@users.noreply.github.com>
2025-01-20 02:50:57 -0800

    fix trailing slash again

--- build/_app/immutable/chunks/paths.Dkbgx-nw.js
+++
@@ -1,1 +0,0 @@
-var s,e;const o=(e=(s=globalThis.__sveltekit_1tqwo4e)==null?void 0:s.base)!=null?e:"";var t,a;const l=(a=(t=globalThis.__sveltekit_1tqwo4e)==null?void 0:t.assets)!=null?a:o;export{l as a,o as b};
